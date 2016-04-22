d64df343a6febac7a0ce5768fb909b7340b04238
Honza Horak <hhorak@redhat.com>
2016-04-22 22:20:57 +0200

    Add rest of postgresql

---
+++ packages/postgresql-common/basic-usage/create-user.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Caution: This is common script that is shared by more packages.
+# If you need to do changes related to this particular collection,
+# create a copy of this file instead of symlink.
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+source ${THISDIR}/../../../common/functions.sh
+
+set -x
+
+[ -z "${PGDATA}" ] && exit_fail "Environment variable PGDATA must be set"
+
+service $SERVICE_NAME restart
+
+su - postgres <<EOF
+  set -x
+  psql -c "CREATE ROLE testuser ENCRYPTED PASSWORD 'secretpass' LOGIN;"
+  createdb testdb --owner testuser;
+EOF
+
+# we can ignore errors until now
+set -e
+
+echo "local all testuser md5" > ${PGDATA}/data/pg_hba.conf
+service $SERVICE_NAME restart
+
