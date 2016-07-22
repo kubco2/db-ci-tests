6e1539c21d00e2a569df02ea452e0a14d2aab1eb
Honza Horak <hhorak@redhat.com>
2016-07-22 08:44:39 +0200

    Initial commit of 1minutetip support

---
+++ 1minutetip/common/run1minutetip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+PACKAGES="koji createrepo git wget vim"
+1minutetip -p "PACKAGES=\"${PACKAGES}\"" 1MT-Fedora24
---
+++ 1minutetip/mariadb/runtest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+
+TEST_REPO_SUBDIR=mariadb
+source ${THISDIR}/../common/runtest.sh
+
+
