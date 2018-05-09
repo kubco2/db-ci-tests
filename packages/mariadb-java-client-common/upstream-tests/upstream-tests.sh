bcebd1bb9ab9a66c5b2fe2089959c50efe06b424
Jakub Janco <jjanco@redhat.com>
2018-05-09 11:01:35 +0200

    Search all available test classes

--- packages/mariadb-java-client-common/upstream-tests/upstream-tests.sh
+++ packages/mariadb-java-client-common/upstream-tests/upstream-tests.sh
@@ -1,90 +1,9 @@
 #!/bin/bash
 
-yum install -y mariadb-java-client mariadb-java-client-tests hamcrest junit mariadb-server
+yum install -y mariadb-java-client mariadb-java-client-tests hamcrest junit mariadb-server unzip
 
-# command to obtain all tests in Java format
-# find org -name '*Test.java' | cut -d'.' -f1 | tr '/' '.'
-RUN_TESTS='org.mariadb.jdbc.CheckDataTest
-org.mariadb.jdbc.ExecuteBatchTest
-org.mariadb.jdbc.TransactionTest
-org.mariadb.jdbc.ServerPrepareStatementTest
-org.mariadb.jdbc.DateTest
-org.mariadb.jdbc.MariaDbCompatibilityTest
-org.mariadb.jdbc.ScrollTypeTest
-org.mariadb.jdbc.StatementTest
-org.mariadb.jdbc.LocalInfileDisableTest
-org.mariadb.jdbc.GeneratedKeysTest
-org.mariadb.jdbc.failover.SequentialFailoverTest
-org.mariadb.jdbc.failover.AuroraFailoverTest
-org.mariadb.jdbc.failover.BaseMultiHostTest
-org.mariadb.jdbc.failover.LoadBalanceFailoverTest
-org.mariadb.jdbc.failover.MonoServerFailoverTest
-org.mariadb.jdbc.failover.CancelTest
-org.mariadb.jdbc.failover.ReplicationFailoverTest
-org.mariadb.jdbc.failover.OldFailoverTest
-org.mariadb.jdbc.failover.AuroraAutoDiscoveryTest
-org.mariadb.jdbc.failover.GaleraFailoverTest
-org.mariadb.jdbc.LocalInfileInputStreamTest
-org.mariadb.jdbc.ConnectionTest
-org.mariadb.jdbc.UnicodeTest
-org.mariadb.jdbc.AllowMultiQueriesTest
-org.mariadb.jdbc.UpdateResultSetTest
-org.mariadb.jdbc.ClientPreparedStatementParsingTest
-org.mariadb.jdbc.DataTypeUnsignedTest
-org.mariadb.jdbc.ResultSetUnsupportedMethodsTest
-org.mariadb.jdbc.ResultSetTest
-org.mariadb.jdbc.DatatypeTest
-org.mariadb.jdbc.GeneratedTest
-org.mariadb.jdbc.TimeoutTest
-org.mariadb.jdbc.CancelTest
-org.mariadb.jdbc.BaseTest
-org.mariadb.jdbc.GiganticLoadDataInfileTest
-org.mariadb.jdbc.DataSourcePoolTest
-org.mariadb.jdbc.MariaDbClobTest
-org.mariadb.jdbc.CallStatementTest
-org.mariadb.jdbc.DistributedTransactionTest
-org.mariadb.jdbc.MariaDbBlobTest
-org.mariadb.jdbc.StoredProcedureTest
-org.mariadb.jdbc.TimezoneDaylightSavingTimeTest
-org.mariadb.jdbc.DataNTypeTest
-org.mariadb.jdbc.CatalogTest
-org.mariadb.jdbc.SslTest
-org.mariadb.jdbc.DriverTest
-org.mariadb.jdbc.GeometryTest
-org.mariadb.jdbc.ComMultiPrepareStatementTest
-org.mariadb.jdbc.ResultSetMetaDataTest
-org.mariadb.jdbc.PasswordEncodingTest
-org.mariadb.jdbc.DatatypeCompatibilityTest
-org.mariadb.jdbc.PooledConnectionTest
-org.mariadb.jdbc.ConnectionPoolTest
-org.mariadb.jdbc.ErrorMessageTest
-org.mariadb.jdbc.PreparedStatementTest
-org.mariadb.jdbc.MultiTest
-org.mariadb.jdbc.UpdateResultSetMethodsTest
-org.mariadb.jdbc.DataTypeSignedTest
-org.mariadb.jdbc.RePrepareTest
-org.mariadb.jdbc.BlobTest
-org.mariadb.jdbc.BasicBatchTest
-org.mariadb.jdbc.MariaDbPoolDataSourceTest
-org.mariadb.jdbc.DataSourceTest
-org.mariadb.jdbc.StateChangeTest
-org.mariadb.jdbc.DatabaseMetadataTest
-org.mariadb.jdbc.JdbcParserTest
-org.mariadb.jdbc.TruncateExceptionTest
-org.mariadb.jdbc.internal.protocol.tls.HostnameVerifierImplTest
-org.mariadb.jdbc.internal.util.DefaultOptionsTest
-org.mariadb.jdbc.internal.util.dao.ClientPrepareResultTest
-org.mariadb.jdbc.internal.util.UtilsTest
-org.mariadb.jdbc.internal.util.SchedulerServiceProviderHolderTest
-org.mariadb.jdbc.internal.util.buffer.BufferTest
-org.mariadb.jdbc.UtilTest
-org.mariadb.jdbc.ParserTest
-org.mariadb.jdbc.FetchSizeTest
-org.mariadb.jdbc.MariaDbDatabaseMetaDataTest
-org.mariadb.jdbc.CollationTest
-org.mariadb.jdbc.BigQueryTest
-org.mariadb.jdbc.BufferTest
-org.mariadb.jdbc.BooleanTest'
+# list all available test classes
+MARIADB_TESTS="$(unzip -Z1 $JAVA_LIBRARY_TESTS | grep 'Test.class' | cut -d'.' -f1 | tr '/' '.' | sort)"
 
 initialize_db() {
 	systemctl start mariadb
@@ -102,8 +21,7 @@ run_test() {
 	JAVA_PATH=$JAVA_PATH:/usr/share/java/hamcrest/core.jar
 	JAVA_PATH=$JAVA_PATH:/usr/share/java/junit.jar
 	# MariaDB
-	JAVA_PATH=$JAVA_PATH:/usr/lib/java/mariadb-java-client.jar
-	JAVA_PATH=$JAVA_PATH:/usr/lib/java/mariadb-java-client-tests.jar
+	JAVA_PATH=$JAVA_PATH:$JAVA_LIBRARY:$JAVA_LIBRARY_TESTS
 	# other dependencies
 	JAVA_PATH=$JAVA_PATH:/usr/share/java/slf4j/slf4j-api.jar
 	JAVA_PATH=$JAVA_PATH:/usr/share/java/slf4j/slf4j-simple.jar
@@ -118,10 +36,11 @@ run_test() {
 
 run_all() {
 	local FAILED=false
+	echo "These tests will run: $MARIADB_TESTS"
 	while read -r test; do
 		echo "Run test: '$test'"
 		run_test $test || FAILED=true
-	done <<< "$RUN_TESTS"
+	done <<< "$MARIADB_TESTS"
 	[ "$FAILED" == "true" ] && {
 	echo '*********************************'
 	echo '************ TESTS HAVE FAILURES!'
